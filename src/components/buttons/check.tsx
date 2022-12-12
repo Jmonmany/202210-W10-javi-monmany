21f2829b7437c3a07eb4c69506db00735b7d7543
Javi <monmany1992@gmail.com>
2022-12-12 16:34:52 +0100

    added check functionality

--- src/components/buttons/check.tsx
+++ src/components/buttons/check.tsx
@@ -1,7 +1,33 @@
-export function CheckButton() {
+import { useState } from 'react';
+import '../../index.css';
+
+export function CheckButton({
+    getGentleman,
+}: {
+    getGentleman: () => void;
+}) {
+    const classState = {
+        check: 'gentleman__icon',
+    };
+
+    const [checkedClass, setCount] = useState(classState);
+
+    const handlerCheck = () => {
+        setCount({
+            ...checkedClass,
+            check: 'gentleman__icon--checked',
+        });
+        getGentleman();
+    };
+
     return (
         <>
-            <i className="icon gentleman__icon fas fa-check"></i>
+            <i
+                className={`icon ${checkedClass.check} fas fa-check`}
+                onClick={() => {
+                    handlerCheck();
+                }}
+            ></i>
         </>
     );
 }
--- src/components/info/info.tsx
+++ src/components/info/info.tsx
@@ -1,8 +1,16 @@
 /* eslint-disable prefer-const */
+import { useState } from 'react';
+import { GentlemanType } from '../../types/gentleman';
 import { SelectAllButton } from '../buttons/select.all';
 
-export function Info() {
+export function Info({ gentlemenData }: { gentlemenData: GentlemanType[] }) {
     let counter = 0;
+    // const [updatedCounter, serCounter] = useState()
+    const countGentlemenSelected = () => {
+        gentlemenData.map((item) => (item.selected ? counter++ : item));
+    };
+    countGentlemenSelected();
+
     return (
         <section className="controls">
             <p className="info">{counter} gentlemen pointing at you</p>
